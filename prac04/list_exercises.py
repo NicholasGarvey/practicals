3a5470dbbde01185c0d8a8bafd5b2ad697bc80b5
Nicholas Garvey <nicholas.garvey@jcu.edu.au>
2019-09-05 11:29:38 +1000

    updated prac04

--- prac04/list_exercises.py
+++ prac04/list_exercises.py
@@ -10,3 +10,12 @@ print("The largest number is", max(numbers))
 print("The average sum of the numbers is", sum(numbers) / len(numbers))
 
 
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45',
+             'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState',
+             'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+username = input("Please enter your username")
+if username in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
