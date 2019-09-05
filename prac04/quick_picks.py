93d66f16d4d22af56c166cd0df1dfb49e7196b63
Nicholas Garvey <nicholas.garvey@jcu.edu.au>
2019-09-05 11:46:08 +1000

    updated prac04

---
+++ prac04/quick_picks.py
@@ -0,0 +1,24 @@
+import random
+
+numbers_per_line = 6
+minimum = 1
+maximum = 45
+
+
+def main():
+    number_of_quick_picks = int(input("How many quick picks"))
+    while number_of_quick_picks < 0:
+        print("That doesn't make sense")
+        number_of_quick_picks = int(input("how many quick picks"))
+
+    for i in range(number_of_quick_picks):
+        quick_pick = []
+        for j in range(numbers_per_line):
+            number = random.randint(minimum, maximum)
+            while number in quick_pick:
+                number = random.randint(minimum, maximum)
+            quick_pick.append(number)
+        print(" ".join("{:2}".format(number) for number in quick_pick))
+
+
+main()
