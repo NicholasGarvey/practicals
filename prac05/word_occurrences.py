f19199a7b8560847edb89680e4e8ea6c7520a6c5
Nicholas Garvey <nicholas.garvey@jcu.edu.au>
2019-09-12 10:44:07 +1000

    finished prac05

--- prac05/word_occurrences.py
+++ prac05/word_occurrences.py
@@ -9,3 +9,9 @@ for word in words:
         word_count[word] = 1
 
 words = list(word_count.keys())
+words.sort()
+
+maximum_length = max((len(word) for word in words))
+
+for word in words:
+    print("{:{}} : {}".format(word, maximum_length, word_count[word]))
