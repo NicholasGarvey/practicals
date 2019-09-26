eaf34f18a4b1290806f1a31c76d71124b5413f0c
Nicholas Garvey <nicholas.garvey@jcu.edu.au>
2019-09-26 11:48:07 +1000

    Completed prac_06

--- prac_06/proggramming_lanuage.py
+++ prac_06/proggramming_lanuage.py
@@ -1,25 +1,25 @@
 class ProgrammingLanguage:
-    """Represent information about a programming language."""
+    # Represent information about a programming language
 
     def __init__(self, name, typing, reflection, year):
-        """Construct's a programminglanguage from values."""
+        # Construct's a programminglanguage from values
         self.name = name
         self.typing = typing
         self.reflection = reflection
         self.year = year
 
     def __str__(self):
-        """Return's string representation of a ProgrammingLanguage."""
+        # Return's string representation of a ProgrammingLanguage
         return "{}, {} Typing, Reflection={}, First appeared in {}".format(
             self.name, self.typing, self.reflection, self.year)
 
     def is_dynamic(self):
-        """Determine's if the language is dynamically typed."""
+        # Determine's if the language is dynamically typed
         return self.typing == "Dynamic"
 
 
 def run_tests():
-    """Run's a simple test on ProgrammingLanguage class."""
+    # Run's a simple test on ProgrammingLanguage class
     ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
     python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
     visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
